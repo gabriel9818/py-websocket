8ec3137ad817acbbc4f2f56779715befe798e0f9
gabriel9818 <garbielruales18@gmail.com>
2024-12-04 04:50:52 -0500

    feat: :sparkles: add client

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ def handle_message(data):
     socketio.emit('respuesta', {'mensaje': mensaje})
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True, host='127.0.0.1', port=5000)
+
